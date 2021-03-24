2c4f535ee7addc2326e470dc38be5d66c1bf8f8c
dts1234 <adas.kazmierczak@gmail.com>
2021-03-24 19:36:45 +0100

    first domain object

---
+++ src/main/java/uep/football/manager/domain/Player.java
@@ -0,0 +1,13 @@
+package uep.football.manager.domain;
+
+/**
+ * @author akazmierczak
+ * @date 24.03.2021
+ */
+public class Player {
+
+    private Long playerId;
+    private String name;
+    private String surname;
+
+}
